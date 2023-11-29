99960d8cddd4e46bf046d64c5574aea69d56354e
Andreas Hofer <andreas.hofer@student.tuwien.ac.at>
2023-11-29 22:50:42 +0100

    add implementations for FromArguments and IntoResult

--- crates/interpreter/src/wdl_std/from_arguments.rs
+++ crates/interpreter/src/wdl_std/from_arguments.rs
@@ -9,10 +9,94 @@ pub(crate) trait FromArguments: Sized {
 impl FromArguments for Value {
 	fn from_args(args: &mut IntoIter<Value>) -> Result<Self, Error> {
 		if let Some(arg) = args.next() {
-			Ok(arg.clone())
+			Ok(arg)
 		} else {
 			// TODO: improve error
-			Err(Error::Fatal("Too few arguments given!".to_owned()))
+			Err(Error::Fatal("Too few arguments given".to_owned()))
+		}
+	}
+}
+
+impl FromArguments for Option<Value> {
+	fn from_args(args: &mut IntoIter<Value>) -> Result<Self, Error> {
+		Ok(args.next())
+	}
+}
+
+impl FromArguments for bool {
+	fn from_args(args: &mut IntoIter<Value>) -> Result<Self, Error> {
+		if let Some(Value::Bool(arg)) = args.next() {
+			Ok(arg)
+		} else {
+			// TODO: improve error
+			Err(Error::Fatal("Error with argument".to_owned()))
+		}
+	}
+}
+
+impl FromArguments for Option<bool> {
+	fn from_args(args: &mut IntoIter<Value>) -> Result<Self, Error> {
+		if let Some(arg) = args.next() {
+			if let Value::Bool(arg) = arg {
+				Ok(Some(arg))
+			} else {
+				// TODO: improve error
+				Err(Error::Fatal("Error with argument".to_owned()))
+			}
+		} else {
+			Ok(None)
+		}
+	}
+}
+
+impl FromArguments for f64 {
+	fn from_args(args: &mut IntoIter<Value>) -> Result<Self, Error> {
+		if let Some(Value::Number(arg)) = args.next() {
+			Ok(arg)
+		} else {
+			// TODO: improve error
+			Err(Error::Fatal("Error with argument".to_owned()))
+		}
+	}
+}
+
+impl FromArguments for Option<f64> {
+	fn from_args(args: &mut IntoIter<Value>) -> Result<Self, Error> {
+		if let Some(arg) = args.next() {
+			if let Value::Number(arg) = arg {
+				Ok(Some(arg))
+			} else {
+				// TODO: improve error
+				Err(Error::Fatal("Error with argument".to_owned()))
+			}
+		} else {
+			Ok(None)
+		}
+	}
+}
+
+impl FromArguments for String {
+	fn from_args(args: &mut IntoIter<Value>) -> Result<Self, Error> {
+		if let Some(Value::String(arg)) = args.next() {
+			Ok(arg)
+		} else {
+			// TODO: improve error
+			Err(Error::Fatal("Error with argument".to_owned()))
+		}
+	}
+}
+
+impl FromArguments for Option<String> {
+	fn from_args(args: &mut IntoIter<Value>) -> Result<Self, Error> {
+		if let Some(arg) = args.next() {
+			if let Value::String(arg) = arg {
+				Ok(Some(arg))
+			} else {
+				// TODO: improve error
+				Err(Error::Fatal("Error with argument".to_owned()))
+			}
+		} else {
+			Ok(None)
 		}
 	}
 }
--- crates/interpreter/src/wdl_std/into_result.rs
+++ crates/interpreter/src/wdl_std/into_result.rs
@@ -4,20 +4,56 @@ pub(crate) trait IntoResult {
 	fn into_result(self) -> Result<Value, Error>;
 }
 
-impl IntoResult for () {
-	fn into_result(self) -> Result<Value, Error> {
-		Ok(Value::Null)
-	}
-}
-
 impl IntoResult for Value {
 	fn into_result(self) -> Result<Value, Error> {
 		Ok(self)
 	}
 }
 
+impl IntoResult for () {
+	fn into_result(self) -> Result<Value, Error> {
+		Ok(Value::Null)
+	}
+}
+
 impl IntoResult for Result<Value, Error> {
 	fn into_result(self) -> Result<Value, Error> {
 		self
+	}
+}
+
+impl IntoResult for bool {
+	fn into_result(self) -> Result<Value, Error> {
+		Ok(Value::Bool(self))
+	}
+}
+
+impl IntoResult for Result<bool, Error> {
+	fn into_result(self) -> Result<Value, Error> {
+		Ok(Value::Bool(self?))
+	}
+}
+
+impl IntoResult for f64 {
+	fn into_result(self) -> Result<Value, Error> {
+		Ok(Value::Number(self))
+	}
+}
+
+impl IntoResult for Result<f64, Error> {
+	fn into_result(self) -> Result<Value, Error> {
+		Ok(Value::Number(self?))
+	}
+}
+
+impl IntoResult for String {
+	fn into_result(self) -> Result<Value, Error> {
+		Ok(Value::String(self))
+	}
+}
+
+impl IntoResult for Result<String, Error> {
+	fn into_result(self) -> Result<Value, Error> {
+		Ok(Value::String(self?))
 	}
 }
