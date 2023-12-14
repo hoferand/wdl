7dbffd808beb5b42496421ba8d48228adc695664
Andreas Hofer <andreas.hofer@student.tuwien.ac.at>
2023-12-14 19:03:38 +0100

    fix multidimensional offsets

--- crates/parser/src/parser/expression/member_call_index.rs
+++ crates/parser/src/parser/expression/member_call_index.rs
@@ -5,64 +5,68 @@ use crate::{Parser, ParserError, TokenValue};
 use super::{parse_atomic, parse_expression};
 
 pub(crate) fn parse_member_call_index(parser: &mut Parser) -> Result<Expression, ParserError> {
-	let expr = parse_atomic(parser)?;
+	let mut expr = parse_atomic(parser)?;
 
-	if let Some(paren) = parser.tokens.want(TokenValue::ParenOpen) {
-		// parse function call
-		let start = paren.span.start.clone();
+	loop {
+		if let Some(paren) = parser.tokens.want(TokenValue::ParenOpen) {
+			// parse function call
+			let start = paren.span.start.clone();
 
-		let mut parameter = Vec::new();
-		while let Some(token) = parser.tokens.peek() {
-			if token.value == TokenValue::ParenClose {
-				break;
+			let mut parameter = Vec::new();
+			while let Some(token) = parser.tokens.peek() {
+				if token.value == TokenValue::ParenClose {
+					break;
+				}
+				parameter.push(parse_expression(parser)?);
+				parser.tokens.want(TokenValue::Comma);
 			}
-			parameter.push(parse_expression(parser)?);
-			parser.tokens.want(TokenValue::Comma);
-		}
 
-		let end = parser
-			.tokens
-			.expect(TokenValue::ParenClose)?
-			.span
-			.end
-			.clone();
+			let end = parser
+				.tokens
+				.expect(TokenValue::ParenClose)?
+				.span
+				.end
+				.clone();
 
-		return Ok(Expression::FunctionCall(Node {
-			span: Span {
-				start: expr.get_span().start.clone(),
-				end: end.clone(),
-			},
-			val: FunctionCall {
-				function: Box::new(expr),
-				parameter: Node {
-					span: Span { start, end },
-					val: parameter,
+			expr = Expression::FunctionCall(Node {
+				span: Span {
+					start: expr.get_span().start.clone(),
+					end: end.clone(),
 				},
-			},
-		}));
-	} else if let Some(bracket) = parser.tokens.want(TokenValue::BracketOpen) {
-		// parse index
-		let start = bracket.span.start.clone();
+				val: FunctionCall {
+					function: Box::new(expr),
+					parameter: Node {
+						span: Span { start, end },
+						val: parameter,
+					},
+				},
+			});
+		} else if let Some(bracket) = parser.tokens.want(TokenValue::BracketOpen) {
+			// parse index
+			let start = bracket.span.start.clone();
 
-		let offset = parse_expression(parser)?;
+			let offset = parse_expression(parser)?;
 
-		let end = parser
-			.tokens
-			.expect(TokenValue::BracketClose)?
-			.span
-			.end
-			.clone();
+			let end = parser
+				.tokens
+				.expect(TokenValue::BracketClose)?
+				.span
+				.end
+				.clone();
 
-		return Ok(Expression::Offset(Node {
-			span: Span { start, end },
-			val: Offset {
-				value: Box::new(expr),
-				offset: Box::new(offset),
-			},
-		}));
-	} else if parser.tokens.want(TokenValue::Point).is_some() {
-		// parse member
-		todo!()
+			expr = Expression::Offset(Node {
+				span: Span { start, end },
+				val: Offset {
+					value: Box::new(expr),
+					offset: Box::new(offset),
+				},
+			});
+		} else if parser.tokens.want(TokenValue::Point).is_some() {
+			// parse member
+			todo!()
+		} else {
+			break;
+		}
 	}
 
 	Ok(expr)
