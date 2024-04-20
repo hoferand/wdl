7cf3e0a0aa52b4e4026d5e915afc069ead2321f6
Andreas Hofer <andreas.hofer@student.tuwien.ac.at>
2024-04-20 16:17:49 +0200

    add named arguments for custom functions

--- crates/interpreter/src/expr/function_call.rs
+++ crates/interpreter/src/expr/function_call.rs
@@ -37,31 +37,73 @@ pub async fn interpret_function_call(
 		)));
 	};
 
+	let mut args = Vec::new();
+	let mut named_args = HashMap::new();
+	for (idx, arg) in expr.val.args.iter().enumerate() {
+		let val = interpret_expr(&arg.val.val, env, g_env).await?;
+
+		if let Some(id) = &arg.val.id {
+			named_args.insert(
+				id.val.clone(),
+				ArgumentValue {
+					idx: idx + 1,
+					span: arg.src.clone(),
+					val,
+				},
+			);
+		} else {
+			args.push(ArgumentValue {
+				idx: idx + 1,
+				span: arg.val.val.get_src().clone(),
+				val,
+			});
+		}
+	}
+
 	let val;
 	match function_val {
 		FunctionValue::Custom(function) => {
 			let inner_env = Arc::new(Environment::with_parent(Arc::clone(g_env)));
 
-			let mut ids = function.parameter.iter();
-			let mut vals = expr.val.args.iter();
-			loop {
-				match (ids.next(), vals.next()) {
-					(None, Some(_)) | (Some(_), None) => {
-						return Err(Error {
-							kind: ErrorKind::ArityMismatch {
-								expected: function.parameter.len(),
-								given: expr.val.args.len(),
-							},
-							src: Some(expr.val.function.get_src().clone()),
-						});
-					}
-					(Some(id_node), Some(val_expr)) => {
-						let id = id_node.val.id.clone();
-						let val = interpret_expr(&val_expr.val.val, env, g_env).await?;
-						inner_env.declare(id, val).await?;
-					}
-					_ => break,
+			let mut vals = args.into_iter();
+			for id in function.parameter.iter() {
+				if let Some(val) = vals.next() {
+					// positional argument
+					inner_env.declare(id.val.id.clone(), val.val).await?;
+				} else if let Some(val) = named_args.get(&id.val.id.val).cloned() {
+					// named argument
+					named_args.remove(&id.val.id.val);
+					inner_env.declare(id.val.id.clone(), val.val).await?;
+				} else {
+					// parameter missing
+					return Err(Error {
+						kind: ErrorKind::MissingArgument {
+							id: id.val.id.val.clone(),
+						},
+						src: Some(expr.val.function.get_src().clone()),
+					});
 				}
+			}
+
+			let rem = vals.count();
+			let rem_named = named_args.keys().len();
+
+			if let Some((id, arg)) = named_args.into_iter().next() {
+				return Err(Error {
+					kind: ErrorKind::UnknownArgument { id },
+					src: Some(arg.span),
+				});
+			}
+
+			if rem != 0 {
+				let expected = function.parameter.len();
+				return Err(Error {
+					kind: ErrorKind::ArityMismatch {
+						expected,
+						given: expected + rem + rem_named,
+					},
+					src: Some(expr.val.function.get_src().clone()),
+				});
 			}
 
 			match stmt::interpret_block(&function.body, &inner_env, g_env).await? {
@@ -76,29 +118,6 @@ pub async fn interpret_function_call(
 			}
 		}
 		FunctionValue::Std(std_fn) => {
-			let mut args = Vec::new();
-			let mut named_args = HashMap::new();
-			for (idx, arg) in expr.val.args.iter().enumerate() {
-				let val = interpret_expr(&arg.val.val, env, g_env).await?;
-
-				if let Some(id) = &arg.val.id {
-					named_args.insert(
-						id.val.clone(),
-						ArgumentValue {
-							idx: idx + 1,
-							span: arg.src.clone(),
-							val,
-						},
-					);
-				} else {
-					args.push(ArgumentValue {
-						idx: idx + 1,
-						span: arg.val.val.get_src().clone(),
-						val,
-					});
-				}
-			}
-
 			let args = args.into_iter();
 			val = std_fn
 				.call_with_ctx(CallContext {
