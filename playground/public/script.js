c02de596a22bd0e6b19314cc438fb05c33639fa1
Andreas Hofer <andreas.hofer@student.tuwien.ac.at>
2024-05-05 22:16:31 +0200

    add playground

---
+++ playground/public/script.js
@@ -0,0 +1,217 @@
+var editor = null;
+
+window.addEventListener("load", (event) => {
+	require.config({
+		paths: { vs: "monaco-editor/min/vs" },
+	});
+
+	require(["vs/editor/editor.main"], function () {
+		define_wdl();
+
+		editor = monaco.editor.create(document.getElementById("editor-container"), {
+			value: default_source,
+			language: "wdl",
+			theme: "wdl-theme",
+			minimap: { enabled: false },
+		});
+
+		editor.getModel().updateOptions({ tabSize: 4 });
+
+		editor.getModel().onDidChangeContent((_event) => {
+			debounced_check(editor.getValue());
+		});
+
+		check(editor.getValue());
+	});
+});
+
+const debounced_check = debounce(check);
+async function check(src) {
+	const response = await fetch("http://localhost:3000/check", {
+		method: "POST",
+		body: src,
+	});
+
+	let json = await response.json();
+
+	let errors = [];
+	if (json.status === "ok") {
+		document.getElementById("output-area").innerText = "No problems found";
+	} else {
+		document.getElementById("output-area").innerText = "";
+		for (error of json.errors) {
+			document.getElementById("output-area").innerText += error.title;
+			if (error.pos) {
+				const pos = error.pos;
+				document.getElementById("output-area").innerText +=
+					"\n" + pos.span_str + "\n";
+
+				errors.push({
+					startLineNumber: pos.span.start.line + 1,
+					startColumn: pos.span.start.column + 1,
+					endLineNumber: pos.span.end.line + 1,
+					endColumn: pos.span.end.column + 1,
+					message: error.title,
+					severity: monaco.MarkerSeverity.Error,
+				});
+			}
+		}
+	}
+	monaco.editor.setModelMarkers(editor.getModel(), "owner", errors);
+}
+
+function debounce(func, timeout = 300) {
+	let timer;
+	return (...args) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			func.apply(this, args);
+		}, timeout);
+	};
+}
+
+const default_source = `global source = "mySource";
+global destination = "myDestination";
+
+actions {
+    action::pickup(
+        target: {
+            stations: [
+                source
+            ]
+        },
+        events: {
+            no_station_left: order::cancel
+        }
+    );
+
+    action::drop(
+        target: {
+            stations: [
+                destination
+            ]
+        }
+    );
+}
+`;
+
+function define_wdl() {
+	monaco.languages.register({
+		id: "wdl",
+		extensions: [".wdl"],
+		aliases: ["WDL", "wdl"],
+		mimetypes: ["application/wdl"],
+	});
+
+	monaco.languages.setMonarchTokensProvider("wdl", {
+		brackets: [
+			{ open: "{", close: "}", token: "delimiter.curly" },
+			{ open: "[", close: "]", token: "delimiter.bracket" },
+			{ open: "(", close: ")", token: "delimiter.parenthesis" },
+		],
+		ignoreCase: false,
+		unicode: true,
+		keywordsControl: [
+			"actions",
+			"if",
+			"else",
+			"while",
+			"continue",
+			"break",
+			"return",
+			"spawn",
+			"par",
+		],
+		keywordsOther: ["global", "function", "let", "true", "false", "null"],
+		operators: [
+			"!",
+			"+",
+			"-",
+			"*",
+			"/",
+			"%",
+			"<",
+			">",
+			"==",
+			"!=",
+			"<=",
+			">=",
+			"and",
+			"or",
+		],
+		tokenizer: {
+			root: [
+				[/(\p{XID_Start}|_)\p{XID_Continue}*(?=::)/, "support.class"],
+				[/(\p{XID_Start}|_)\p{XID_Continue}*(?=\()/, "support.function"],
+				[
+					/(\p{XID_Start}|_)\p{XID_Continue}*\b/,
+					{
+						cases: {
+							"@keywordsControl": "keyword.control",
+							"@keywordsOther": "keyword.other",
+							"@operators": "operator",
+							"@default": "variable",
+						},
+					},
+				],
+				{ include: "@comment" },
+				{ include: "@string" },
+			],
+			comment: [
+				[/\/\/.*$/, "comment"],
+				[/\/\*/, "comment", "@comment_body"],
+			],
+			comment_body: [
+				[/\*\//, "comment", "@pop"],
+				[/./, "comment"],
+			],
+			string: [[/"/, "string", "@string_body"]],
+			string_body: [
+				[/"/, "string", "@pop"],
+				[/./, "string"],
+			],
+		},
+	});
+
+	monaco.languages.setLanguageConfiguration("wdl", {
+		comments: { lineComment: "//", blockComment: ["/*", "*/"] },
+		brackets: [
+			["{", "}"],
+			["[", "]"],
+			["(", ")"],
+		],
+		surroundingPairs: [
+			{ open: "{", close: "}" },
+			{ open: "[", close: "]" },
+			{ open: "(", close: ")" },
+			{ open: '"', close: '"' },
+		],
+		autoClosingPairs: [
+			{ open: "{", close: "}" },
+			{ open: "[", close: "]" },
+			{ open: "(", close: ")" },
+			{ open: '"', close: '"', notIn: ["string", "comment"] },
+			{ open: "/*", close: "*/", notIn: ["string", "comment"] },
+		],
+	});
+
+	monaco.editor.defineTheme("wdl-theme", {
+		base: "vs",
+		inherit: true,
+		colors: {},
+		rules: [
+			{
+				token: "keyword.control",
+				foreground: "#AF00DB",
+			},
+			{
+				token: "support.class",
+				foreground: "#267F99",
+			},
+			{
+				token: "support.function",
+				foreground: "#795d26",
+			},
+		],
+	});
+}
---
+++ playground/src/main.rs
@@ -0,0 +1,255 @@
+use ast::{Location, Span};
+use axum::{routing::post, Json, Router};
+use serde::Serialize;
+use serde_json::{json, Value};
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() {
+	let app = Router::new()
+		.route("/check", post(check))
+		.nest_service(
+			"/monaco-editor",
+			ServeDir::new("node_modules/monaco-editor"),
+		)
+		.nest_service("/", ServeDir::new("public"));
+
+	let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+	axum::serve(listener, app).await.unwrap();
+}
+
+async fn check(src_code: String) -> Json<Value> {
+	if let Err(error) = parser::get_ast(&src_code) {
+		Json(json!({
+			"status": "error",
+			"errors": convert_parser_error(error, &src_code)
+		}))
+	} else {
+		Json(json!({
+			"status": "ok"
+		}))
+	}
+}
+
+#[derive(Debug, Serialize)]
+#[serde(tag = "type")]
+struct Error {
+	title: String,
+	pos: Option<Position>,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(tag = "type")]
+struct Position {
+	span: Span,
+	span_str: String,
+}
+
+fn convert_parser_error(error: parser::Error, src_code: &str) -> Vec<Error> {
+	let mut ret = Vec::new();
+	match error {
+		parser::Error::Lexer(errors) => {
+			for err in errors {
+				ret.push(match err {
+					parser::LexerError::InvalidCharacter { char, loc } => {
+						let span = Span {
+							start: loc,
+							end: Location {
+								line: loc.line,
+								column: loc.column + 1,
+							},
+						};
+
+						Error {
+							title: format!("Invalid character `{}` found!", char),
+							pos: Some(Position {
+								span,
+								span_str: create_error_location(&span.start, &span.end, src_code),
+							}),
+						}
+					}
+					parser::LexerError::InvalidNumber { src, span } => Error {
+						title: format!("Invalid number `{}` found!", src),
+						pos: Some(Position {
+							span,
+							span_str: create_error_location(&span.start, &span.end, src_code),
+						}),
+					},
+					parser::LexerError::UnexpectedEndOfFile => {
+						let last_line = src_code.lines().last().unwrap_or("").to_owned();
+						let start = Location {
+							line: src_code.lines().count() - 1,
+							column: last_line.chars().count(),
+						};
+						let span = Span {
+							start,
+							end: Location {
+								line: start.line,
+								column: start.column + 1,
+							},
+						};
+
+						Error {
+							title: format!("Unexpected end of file!"),
+							pos: Some(Position {
+								span,
+								span_str: create_error_location(&span.start, &span.end, src_code),
+							}),
+						}
+					}
+					parser::LexerError::InvalidEscape { char, loc } => {
+						let span = Span {
+							start: Location {
+								line: loc.line,
+								column: loc.column - 2,
+							},
+							end: Location {
+								line: loc.line,
+								column: loc.column,
+							},
+						};
+
+						Error {
+							title: format!("Invalid character escape `\\{}`!", char),
+							pos: Some(Position {
+								span,
+								span_str: create_error_location(&span.start, &span.end, src_code),
+							}),
+						}
+					}
+				})
+			}
+		}
+		parser::Error::Parser(err) => ret.push(match err {
+			parser::ParserError::Fatal(msg) => Error {
+				title: format!("{}!", msg),
+				pos: None,
+			},
+			parser::ParserError::Positional { msg, span } => Error {
+				title: format!("{}!", msg),
+				pos: Some(Position {
+					span,
+					span_str: create_error_location(&span.start, &span.end, src_code),
+				}),
+			},
+			parser::ParserError::UnexpectedToken {
+				src,
+				span,
+				expected,
+			} => {
+				let mut msg = format!("Unexpected token `{}`", src);
+				if expected.len() == 1 {
+					msg += &format!(", expected `{}`", expected[0]);
+				} else if !expected.is_empty() {
+					msg += ", expected one of [";
+					msg += &expected
+						.iter()
+						.map(|e| format!("`{}`", e))
+						.collect::<Vec<String>>()
+						.join(", ");
+					msg += "]";
+				}
+				Error {
+					title: format!("{}!", msg),
+					pos: Some(Position {
+						span,
+						span_str: create_error_location(&span.start, &span.end, src_code),
+					}),
+				}
+			}
+			parser::ParserError::SecondActions {
+				actions1: _,
+				actions2,
+			} => Error {
+				title: format!("Only one `actions` block is allowed!"),
+				pos: Some(Position {
+					span: actions2,
+					span_str: create_error_location(&actions2.start, &actions2.end, src_code),
+				}),
+			},
+			parser::ParserError::ExpectedSemicolon { span } => Error {
+				title: format!("Expected semicolon `;`!"),
+				pos: Some(Position {
+					span,
+					span_str: create_error_location(&span.start, &span.end, src_code),
+				}),
+			},
+			parser::ParserError::UnexpectedEoF => {
+				let last_line = src_code.lines().last().unwrap_or("").to_owned();
+				let start = Location {
+					line: src_code.lines().count() - 1,
+					column: last_line.chars().count(),
+				};
+				let span = Span {
+					start,
+					end: Location {
+						line: start.line,
+						column: start.column + 1,
+					},
+				};
+
+				Error {
+					title: format!("ERROR: Unexpected end of file!"),
+					pos: Some(Position {
+						span,
+						span_str: create_error_location(&span.start, &span.end, src_code),
+					}),
+				}
+			}
+		}),
+	}
+
+	ret
+}
+
+fn create_error_location(start: &Location, end: &Location, src: &str) -> String {
+	let mut ret = String::new();
+
+	let mut lines: Vec<&str> = src.lines().collect();
+	lines.push("");
+
+	let number_padding = (end.line + 1).to_string().len();
+	ret += &format!("{:>pad$}\n", "|", pad = number_padding + 2);
+
+	let show_lines = 3;
+	let mut skipped = false;
+	for line_number in start.line..=end.line {
+		if (end.line - start.line) > show_lines * 2
+			&& !(line_number - start.line < show_lines || end.line - line_number < show_lines)
+		{
+			if !skipped {
+				skipped = true;
+				ret += &format!("{:>pad$} {}\n", "|", "...", pad = number_padding + 2);
+			}
+		} else if let Some(line) = lines.get(line_number) {
+			ret += &format!(
+				"{:<pad$} {:} {}\n",
+				(line_number + 1).to_string(),
+				"|",
+				line,
+				pad = number_padding,
+			);
+		} else {
+			ret = format!(
+				"Internal error, line number `{}` does not exist!",
+				line_number + 1
+			);
+			return ret;
+		}
+	}
+
+	if start.line == end.line {
+		// TODO: fix tab ident
+		ret += &format!(
+			"{:>pad$} {}{}\n",
+			"|",
+			" ".repeat(start.column),
+			"^".repeat(end.column - start.column),
+			pad = number_padding + 2
+		);
+	}
+
+	ret += &format!("{:>pad$}", "|", pad = number_padding + 2);
+
+	ret
+}
