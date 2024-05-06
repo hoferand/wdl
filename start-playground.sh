218a7a92edad7e37fbd7367f61acbf1e469ac9ff
Andreas Hofer <andreas.hofer@student.tuwien.ac.at>
2024-05-06 22:21:01 +0200

    update playground start script

--- start-playground.sh
+++ start-playground.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # build wasm functions
+echo "build wasm..."
 cd crates/wasm
 wasm-pack build -t web -d ../../lang-playground/wasm --release
 cd ../..
 
 # install npm packages
+echo "install npm packages..."
 cd lang-playground
 npm install
 cd ..
 
 # start server
-cargo run --bin server
+echo "start server..."
+cargo run --bin server --release
 
 # open `localhost:3000/index.html` in your browser
